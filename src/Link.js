55d17ac96ef087231c806964cfa5649aadf16c44
keshawn0998 <keshawn@contenda.co>
2022-01-28 06:32:51 -0500

    k

--- src/Link.js
+++ src/Link.js
@@ -14,9 +14,9 @@ class Link extends Component {
 
 
   componentDidMount = async () =>{
-    if (localStorage.getItem("accessToken")){
-      window.location.replace(`https://user-stock-funds.web.app/${localStorage.getItem('accessToken')}`)
-    }
+    // if (localStorage.getItem("accessToken")){
+    //   window.location.replace(`https://user-stock-funds.web.app/${localStorage.getItem('accessToken')}`)
+    // }
     // else{
     alert("This is a stimulation as of right now. Select any bank and use these credentials : {username:'user_good',password:'pass_good'} real bank data coming very soon.")
     var response = await axios.post("https://immense-ridge-09781.herokuapp.com/link_token").then()
